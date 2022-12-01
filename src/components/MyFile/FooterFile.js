2fd82eabb35215873301864ea91705589654b63a
sushmaGit6281 <sushmagandrathi@gmail.com>
2022-12-01 14:46:05 +0530

    footerfile changed

--- src/components/MyFile/FooterFile.js
+++ src/components/MyFile/FooterFile.js
@@ -2,16 +2,16 @@
 import React from 'react'
 function FooterFile() {
     return <div className="Form">
-        <div>
-
+        
+            <footer>
             <ul>
                 <li><a href="#">Privacy</a></li>
                 <li><a href="#">Terms</a></li>
                 <li><a href="#">Contact</a></li>
             </ul>
-            <p>Copyright 2016,Original Trombones</p>
-
-        </div>
+            <span>Copyright 2016,Original Trombones</span>
+            </footer>
+        
     </div>
 }
 export default FooterFile
